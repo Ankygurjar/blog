de2681d69dbb3d25a5f6f5c73a6bc9157e62043d
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2020-06-13 16:10:45 -0400

    my new commit after comment

--- routes/blog.js
+++ routes/blog.js
@@ -54,16 +54,18 @@ router.post('/addBlog', upload.single('picture'),(req, res)=>{
 
   var dateTime = today.getFullYear()+"-"+(today.getMonth()+1)+"-"+today.getDate()+"  "+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds()
 
+  console.log(req.file)
     let blogData = {
       blog_name: req.body.blog_name,
       blog_body: req.body.blog_body,
       by: req.body.by,
-      picture: req.file.filename,
+      picture: req.file.destination+"/"+req.file.filename,
+      category: req.body.category
     }
-
+    console.log(blogData.picture)
     if( blogData.blog_name!= '' && blogData.blog_body!= '' && blogData.by!= '' && blogData.picture!= '' ){
-      const query = "INSERT INTO blog(blog_name, blog_body, by, picture) VALUES($1, $2, $3, $4)"
-      pool.query(query, [blogData.blog_name, blogData.blog_body, blogData.by, blogData.picture])
+      const query = "INSERT INTO blog(blog_name, blog_body, by, picture, category) VALUES($1, $2, $3, $4, $5)"
+      pool.query(query, [blogData.blog_name, blogData.blog_body, blogData.by, blogData.picture, blogData.category])
         .then((data)=>{
           res.status(200).json({status:"Blog has been added"})
         })
@@ -85,12 +87,13 @@ router.put('/updateBlog/:id', upload.single('picture'),(req, res)=>{
     blog_name: req.body.blog_name,
     blog_body: req.body.blog_body,
     by: req.body.by,
+    category: req.body.category,
     picture: req.file.filename,
   }
 
   if(updateBlog.blog_name != '' && updateBlog.blog_body != '' && updateBlog.by != '' && updateBlog.picture != ''){
           pool.query("UPDATE blog SET blog_name = $1, blog_body = $2, by = $3, picture = $4 WHERE blog_id=$5",
-              [updateBlog.blog_name, updateBlog.blog_body, updateBlog.by, updateBlog.picture, id])
+              [updateBlog.blog_name, updateBlog.blog_body, updateBlog.by, updateBlog.picture, updateBlog.category, id])
             .then(data=>{
               res.status(200).json({status:"Blog has been updated"})
             })
@@ -117,5 +120,57 @@ router.delete('/deleteBLog/:id', (req, res)=>{
     res.status(400).json("No Blog with this id is found")
   }
 })
+
+router.post('/addComment', (req, res)=>{
+  const comment = {
+    comment_body : req.body.comment_body,
+    commentor : req.body.commentor,
+    comment_email : req.body.comment_email,
+    blog_id : req.body.blog_id
+  }
+
+  console.log(req.body)
+
+  if(comment.comment_body != '' && comment.commentor != '' && comment.comment_email!= '' && comment.blog_id != ''){
+    const query = 'INSERT INTO blogcomment(comment_body, commentor, comment_email, blog_id) VALUES($1, $2, $3, $4)'
+    pool.query(query, [comment.comment_body, comment.commentor, comment.comment_email, comment.blog_id])
+      .then(data=>{
+        res.status(200).json({status: 'Comment saved '})
+      })
+      .catch(err=>{
+        res.status(400).json(err)
+      })
+  }else{
+    res.staus(400).json({status: 'Please enter all the deatils'})
+  }
+
+})
+
+router.get('/getComments/:blogId', (req, res)=>{
+  const blogId = req.params.blogId
+
+  const query = 'SELECT * FROM blogcomment WHERE blog_id = $1'
+  pool.query(query, [blogId])
+    .then(data=>{
+      res.status(200).json(data.rows)
+    })
+    .catch(err=>{
+      res.status(400).json(err)
+    })
+})
+
+router.delete('/deleteComment/:blogId', (req, res)=>{
+  const blogId = req.params.blogId
+
+  const query = "DELETE FROM blogcomment WHERE comment_id = $1"
+  pool.query(query, [blogId])
+    .then((data)=>{
+      res.status(200).json({status:"Comment with id : "+blogId+" is deleted"})
+    })
+    .catch(err=>{
+      res.status(400).json(err)
+    })
+})
+
 
 module.exports = router
