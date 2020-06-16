60cde8dbcd5efc1ea677212f95813c3346223226
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2020-06-15 22:50:43 -0400

    commit after server port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-const PORT = process.env.PORT = 5000
+const PORT = process.env.PORT = 5065
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
